88f8d391ed65a073449650d8d693338d35b6da30
Extreme <Extreme@192.168.1.102>
2024-03-19 20:52:22 -0600

    Finalizacion semana 10

---
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -0,0 +1,7 @@
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+
+    Usuario findByUsername(String username);
+}
